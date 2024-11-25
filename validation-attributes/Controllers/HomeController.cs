56063ced7c8d2869c8db1db85e96b8c10efed864
pverma2905 <pverma2905@gmail.com>
2024-11-25 10:13:47 +0530

    Add project files.

---
+++ validation-attributes/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using validation_attributes.Models;
+
+namespace validation_attributes.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Student std)
+        {
+            return View();
+            /*if (ModelState.IsValid)
+            {
+                return "Name is: " + std.Name;
+            }
+            else {
+                return "Validation Failed";
+            }*/
+            
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
